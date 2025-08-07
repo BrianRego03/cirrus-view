66dfa6f5eb4a7af4eaac4f502f1bd8aae379264b
brianrego03 <brian.rego03@gmail.com>
2025-08-07 11:30:50 +0530

    Setup routing in Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import router from './components/Routers'
 
 function App() {
   
 
   return (
     <>
-      <div>Hey</div>
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/components/Routers.jsx
+++ src/components/Routers.jsx
@@ -1,1 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import SignUp from "./SignUp";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <Dashboard />,
+        children:[
+            {path:"/signup",element: <SignUp />}
+        ],
+    },
+]);
+
+export default router;
