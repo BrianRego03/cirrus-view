d68d39c27c7b1ca214f123b57d2ac0ffe1481c55
brianrego03 <brian.rego03@gmail.com>
2025-08-07 10:51:47 +0530

    Install react router and create basic router and dashboard components

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+const Dashboard = ()=>{
+    
+
+    return(
+        <>
+        <div>Cirrusview</div>
+        <div><button>Set Windows</button></div>
+        <div><button>View Window status</button></div>
+        <div><button>Log Laundry</button></div>
+        <div><button>Sign up</button></div>
+        </>
+    )
+}
