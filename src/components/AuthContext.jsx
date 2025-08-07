3d02f64f9976ded8b70dc5cd69af38bcef4ed714
brianrego03 <brian.rego03@gmail.com>
2025-08-07 16:49:55 +0530

    Use context api to store auth status

---
+++ src/components/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext,useEffect,useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children})=>{
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+
+    useEffect(()=>{
+        fetch("http://localhost:3000/userState",{
+            credentials:"include",
+        })
+        .then((res)=>{
+            if(res.ok) return res.json();
+            throw new Error("not logged in");
+        })
+        .then((data)=>{
+            setUser(data);
+
+        })
+        .catch(()=>{
+            setUser(null);
+        })
+        .finally(()=>{
+            setLoading(false);
+        });
+    },[]);
+
+    return (
+        <AuthContext.Provider value={{user,setUser,loading}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth =()=>useContext(AuthContext);
