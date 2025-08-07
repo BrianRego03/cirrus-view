38d49cf0274937e991591a9214138f29f43e8026
brianrego03 <brian.rego03@gmail.com>
2025-08-07 11:24:03 +0530

    Create sign up form component

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+
+
+const SignUp=()=>{
+
+    const [formData,setFormData] = useState({username:"",password:""});
+
+    const handleChange = (e) => {
+        setFormData((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+
+        try{
+            const res= await fetch("http://localhost:3000/register",{
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                credentials: "include",
+                body: JSON.stringify(formData),
+
+            });
+
+            const data = await res.json();
+            console.log(res);
+
+            if (res.ok) {
+                alert("User registered successfully!");
+            } else {
+                alert(`Error: ${data.message || "Something went wrong"}`);
+            }
+        } catch (err){
+            console.error(err);
+            alert("Network error");
+        }
+
+        
+    }
+
+
+
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <div>Sign up</div>
+          <div>Enter username</div>
+          <input type="text" name="username" value={formData.username} onChange={handleChange}></input>
+          <div>Enter password</div>
+          <input type="text" name="password" value={formData.password} onChange={handleChange}></input>
+          <button type="submit">Submit</button>
+        </form>
+      </>
+    );
+};
+
+export default SignUp;
