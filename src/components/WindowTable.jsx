6a334f959db58781e4fcb5b437e0d362f13f8c98
brianrego03 <brian.rego03@gmail.com>
2025-08-16 16:34:17 +0530

    Add more row data to window table

--- src/components/WindowTable.jsx
+++ src/components/WindowTable.jsx
@@ -18,13 +18,29 @@ const WindowTable=({windowObject})=>{
         return stringOfTime;
     }
 
+    const dayProvider=(dayString)=>{
+          const d = new Date(dayString + "T00:00:00"); 
+          const ord = (n) => {
+            const s = ["th", "st", "nd", "rd"],
+              v = n % 100;
+            return n + (s[(v - 20) % 10] || s[v] || s[0]);
+          };
+          const weekday = d.toLocaleDateString("en-GB", { weekday: "long" });
+          const month = d.toLocaleDateString("en-GB", { month: "long" });
+          return `${weekday} ${ord(d.getDate())} ${month}, ${d.getFullYear()}`;
+
+    }
+
 
     return(
         <>
             <div className="windowTableContainer">
                 {
                     windowObject.map((windowItem,windowIndex)=>{
-                        return(<div className="windowFrame" key={windowIndex} >
+                        return(
+                        
+                        <div className="windowFrame" key={windowIndex} >
+                            <div className="frameDivSticky">{dayProvider(windowItem.date)}</div>
                             <table className="windowTable">
                             <thead>
 
@@ -41,22 +57,47 @@ const WindowTable=({windowObject})=>{
                             <tbody>
 
                                 <tr>
-                                    <th className="frameSticky" rowSpan={2}>Weather</th>
+                                    <th className="frameSticky" >Rain</th>
                                     {windowItem.windowHours.map((item,index)=>{
                                         return (
                                             <td key={index} className={item.rain?"rainCell":""}>
-                                                {(item.rain)?"rain":"clear"}
+                                                {(item.rain)?"Rain":"Clear"}
                                             </td>
                                         )
                                     })}
 
                                 </tr>
                                 <tr>
-                                    
+                                    <th className="frameSticky" >Precip.</th>
+                                    {windowItem.windowHours.map((item,index)=>{
+                                        return (
+                                            <td key={index}  className={item.rain?"rainCell":""}>
+                                                {item.precip}
+                                            </td>
+                                        )
+                                    })}
+
+                                </tr>
+                                <tr>
+                                    <th className="frameSticky" >Temp</th>
                                     {windowItem.windowHours.map((item,index)=>{
                                         return (
                                             <td key={index}  className={item.rain?"rainCell":""}>
                                                 {item.temp}°C
+                                            </td>
+                                        )
+                                    })}
+
+                                </tr>
+                                <tr>
+                                    <th className="frameSticky" >Status</th>
+                                    {windowItem.windowHours.map((item,index)=>{
+                                        return (
+                                            <td key={index}  className={item.rain?"rainCell":""}>
+                                                <img 
+                                                src={`https://raw.githubusercontent.com/visualcrossing/WeatherIcons/refs/heads/main/PNG/2nd%20Set%20-%20Color/${item.icon}.png`}>
+
+                                                </img>
                                             </td>
                                         )
                                     })}
