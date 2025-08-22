4346184995c328ea7b8b213ab2e4f4943724ebc7
brianrego03 <brian.rego03@gmail.com>
2025-08-22 17:07:15 +0530

    Remove extra code

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,73 +8,9 @@ const Dashboard = ()=>{
     const navigate =useNavigate();
     return (
       <>
-        {/* <div>Cirrusview</div> */}
         <HeaderSub />
 
-        <div>
-          <span>
-            <button
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              Dashboard
-            </button>
-          </span>
-          <span>
-            <button
-              onClick={()=>{
-                navigate("/createLaundry");
-              }}>Set Laundry</button>
-          </span>
-          <span>
-            <button
-              onClick={() => {
-                navigate("/laundry");
-              }}
-            >
-              View Laundry
-            </button>
-          </span>
-          <span>
-            <button>Log Laundry</button>
-          </span>
-          {!user && (
-            <>
-              <span>
-                <button
-                  onClick={() => {
-                    navigate("/signup");
-                  }}
-                >
-                  Sign up
-                </button>
-              </span>
-              <span>
-                <button
-                  onClick={() => {
-                    navigate("/login");
-                  }}
-                >
-                  Log in
-                </button>
-              </span>
-            </>
-          )}
-          {user && (
-            <>
-              <span>
-                <button
-                  onClick={() => {
-                    navigate("/logout");
-                  }}
-                >
-                  Log out
-                </button>
-              </span>
-            </>
-          )}
-        </div>
+   
 
         <div className="outletContainer">
           <Outlet />
